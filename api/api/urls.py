332b88efed47cbf935877708b62fd3c2cd2b09e5
morskmorsk <khalidfour@yahoo.com>
2023-05-03 06:11:36 -0600

    serialize custom user

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts.routers import router as accounts_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'api/', include('accounts.urls')),
+    path(r'accounts/', include('accounts.urls')),
+    path('api/', include(accounts_router.urls)),
 ]
